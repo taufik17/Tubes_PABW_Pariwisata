48574476ecd847cb7dfa278c2c6188e0ca5c3d39
Ahmad Reza Aidil Adha <44763207+rezzaaidil07@users.noreply.github.com>
2018-11-19 17:03:41 +0700

    Add files via upload

---
+++ folder upload/pariwisata/application/views/web/tampilan_tentang.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Tentang</title>
+    <meta charset="utf-8">
+
+    <!-- Font google <-->
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/animate.css">
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/aos.css">
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/flaticon.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/icomoon.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/style.css">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div class="container">
+        <a href="<?php echo base_url();?>" class="navbar-brand">
+          <img src="<?php echo base_url();?>asset/images/logo.png" width="50%" alt="TUBES PABW">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="oi oi-menu"></span> Menu
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item control"><a href="<?php echo base_url(); ?>" class="nav-link icon-home">&nbspBeranda</a></li>
+            <li class="nav-item control"><a href="<?php echo base_url(); ?>Wisata" class="nav-link icon-map">&nbspWisata</a></li>
+            <li class="nav-item control"><a href="<?php echo base_url(); ?>Hotel" class="nav-link icon-hotel">&nbspHotel</a></li>
+            <li class="nav-item control"><a href="<?php echo base_url(); ?>Kuliner" class="nav-link icon-cutlery">&nbspKuliner</a></li>
+            <li class="nav-item control active"><a href="<?php echo base_url(); ?>Tentang" class="nav-link icon-info">&nbspTentang</a></li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- END nav -->
+
+    <div class="hero-wrap js-fullheight" style="background-image: url('<?php echo base_url();?>asset/images/tangan.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
+          <div style="background-color: rgba(1, 68, 64, 0.59)" class="col-md-9 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+            <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><strong>&nbsp&nbspTak Kenal Maka Tak Sayang <br></strong>
+               &nbsp&nbspWe We Are Love You</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+ <div class="album py-5 bg-light">
+        <div class="container">
+
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer ng text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  </div>
+                </div>
+              </div>
+            </div>            
+          </div>
+        </div>
+      </div>
+
+
+
+
+    
+
+    <!-- footer -->
+    <?php $this->load->view('web/tampilan_footer'); ?>
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="<?php echo base_url();?>asset/js/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/popper.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/bootstrap.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.easing.1.3.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.waypoints.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.stellar.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/owl.carousel.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.magnific-popup.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/aos.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.animateNumber.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/bootstrap-datepicker.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.timepicker.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/scrollax.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/google-maps-aes.js"></script>
+  <script src="<?php echo base_url();?>asset/js/google-map.js"></script>
+  <script src="<?php echo base_url();?>asset/js/main.js"></script>
+
+  </body>
+</html>
