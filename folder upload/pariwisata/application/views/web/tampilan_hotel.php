d32ea36e43ff2802cb9e5c3dec9fed0791d09099
Ahmad Reza Aidil Adha <44763207+rezzaaidil07@users.noreply.github.com>
2018-11-22 20:16:43 +0700

    Add files via upload

---
+++ folder upload/pariwisata/application/views/web/tampilan_hotel.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Hotel</title>
+    <meta charset="utf-8">
+
+    <!-- Font google <-->
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/animate.css">
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/aos.css">
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/flaticon.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/icomoon.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/style.css">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div class="container">
+        <a href="<?php echo base_url();?>" class="navbar-brand">
+          <img src="<?php echo base_url();?>asset/images/logo.png" width="50%" alt="TUBES PABW">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="oi oi-menu"></span> Menu
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item control"><a href="<?php echo base_url(); ?>" class="nav-link icon-home">&nbspBeranda</a></li>
+            <li class="nav-item control"><a href="<?php echo base_url(); ?>Wisata" class="nav-link icon-map">&nbspWisata</a></li>
+            <li class="nav-item control active"><a href="<?php echo base_url(); ?>Hotel" class="nav-link icon-hotel">&nbspHotel</a></li>
+            <li class="nav-item control"><a href="<?php echo base_url(); ?>Kuliner" class="nav-link icon-cutlery">&nbspKuliner</a></li>
+            <li class="nav-item control"><a href="<?php echo base_url(); ?>Tentang" class="nav-link icon-info">&nbspTentang</a></li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- END nav -->
+
+    <div class="hero-wrap js-fullheight" style="background-image: url('<?php echo base_url();?>asset/images/bg-hotel.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
+          <div style="background-color: rgba(99, 68, 64, 0.59)" class="col-md-9 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+            <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><strong>&nbsp&nbspHotel terbaik untuk Anda <br></strong>
+               &nbsp&nbspWe Bring The Best for You</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="ftco-section ftco-degree-bg">
+      <div class="container">
+        <h2 class="mb-4"><strong>Hotel</strong></h2>
+        <div class="row">
+        	<div class="col-lg-3 sidebar">
+        		<div class="sidebar-wrap bg-light ftco-animate">
+        			<h3 class="heading mb-4">Temukan Hotel</h3>
+        			<form action="#">
+        				<div class="fields">
+		              <div class="form-group">
+                    <div class="select-wrap one-third">
+                      <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                      <select name="lokasi" id="" class="form-control" placeholder="Keyword search">
+                        <option value="">--Lokasi--</option>
+                        <?php foreach ($tempat ->result() as $tmpt ) { ?>
+                        <option value="<?php echo $tmpt->id_daerah; ?>"><?php echo $tmpt->nama_daerah ?></option>
+                        <?php } ?>
+                      </select>
+                    </div>
+		              </div>
+		              <div class="form-group">
+		                <input type="text" id="checkin_date" class="form-control" placeholder="Date from">
+		              </div>
+		              <div class="form-group">
+		                <input type="text" id="checkin_date" class="form-control" placeholder="Date to">
+		              </div>
+		              <div class="form-group">
+		                <input type="submit" value="Cari" class="btn btn-primary py-3 px-5">
+		              </div>
+		            </div>
+	            </form>
+        		</div>
+          </div>
+          <div class="col-lg-9">
+          	<div class="row">
+              <?php foreach ($hotel->result() as $nhotel ) {
+                ?>
+		    			<div class="col-md-4 ftco-animate">
+		    				<div class="destination">
+		    					<a href="<?php echo base_url();?>hotel/pos/<?php echo $nhotel->id_hotel; ?>" class="img img-2 d-flex justify-content-center align-items-center" style="background-image: url(<?php echo base_url();?>asset/images/<?php echo $nhotel->gambar; ?>);">
+		    						<div class="icon d-flex justify-content-center align-items-center">
+    							<span class="icon-search2"></span>
+    						</div>
+		    					</a>
+		    					<div class="text p-3">
+		    						<div class="d-flex">
+		    							<div class="one">
+				    						<h3><a href="<?php echo base_url();?>hotel/pos/<?php echo $nhotel->id_hotel; ?>"><?php echo $nhotel->nama_hotel; ?></a></h3>
+				    						<p class="rate">
+				    							<i class="icon-star"></i>
+				    							<i class="icon-star"></i>
+				    							<i class="icon-star"></i>
+				    							<i class="icon-star"></i>
+				    							<i class="icon-star-o"></i>
+				    						</p>
+			    						</div>
+			    						<div class="two">
+			    							<span class="price per-price"><?php echo $nhotel->harga; ?></span>
+		    							</div>
+		    						</div>
+		    						<p><?php echo $nhotel->lokasi; ?></p>
+		    						<hr>
+		    						<p class="bottom-area d-flex">
+		    							<span><i class="icon-map-o"></i> <?php echo $nhotel->tempat; ?></span>
+		    							<span class="ml-auto"><a href="<?php echo base_url();?>hotel/pos/<?php echo $nhotel->id_hotel; ?>">Booking</a></span>
+		    						</p>
+		    					</div>
+		    				</div>
+		    			</div>
+              <?php } ?>
+          	</div>
+            <!--
+          	<div class="row mt-5">
+		          <div class="col text-center">
+		            <div class="block-27">
+		              <ul>
+		                <li><a href="#">&lt;</a></li>
+		                <li class="active"><span>1</span></li>
+		                <li><a href="#">2</a></li>
+		                <li><a href="#">3</a></li>
+		                <li><a href="#">4</a></li>
+		                <li><a href="#">5</a></li>
+		                <li><a href="#">&gt;</a></li>
+		              </ul>
+		            </div>
+		          </div>
+		        </div>
+          -->
+          </div> <!-- .col-md-8 -->
+        </div>
+      </div>
+    </section> <!-- .section -->
+
+    <!-- footer -->
+    <?php $this->load->view('web/tampilan_footer'); ?>
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="<?php echo base_url();?>asset/js/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/popper.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/bootstrap.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.easing.1.3.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.waypoints.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.stellar.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/owl.carousel.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.magnific-popup.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/aos.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.animateNumber.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/bootstrap-datepicker.js"></script>
+  <script src="<?php echo base_url();?>asset/js/jquery.timepicker.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/scrollax.min.js"></script>
+  <script src="<?php echo base_url();?>asset/js/google-maps-aes.js"></script>
+  <script src="<?php echo base_url();?>asset/js/google-map.js"></script>
+  <script src="<?php echo base_url();?>asset/js/main.js"></script>
+
+  </body>
+</html>
---
+++ folder upload/pariwisata/application/views/web/tampilan_rincian_hotel.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html"> </html>
+<head>
+    <title>Hotel Praba</title>
+    <meta charset="utf-8">
+
+    <!-- Font google <-->
+
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/animate.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/bootstrap.min1.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/magnific-popup.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/aos.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/jquery.timepicker.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/flaticon.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/icomoon.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>asset/css/style.css">
+
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark" id="ftco-navbar">
+    <div class="container">
+        <a href="<?php echo base_url();?>" class="navbar-brand">
+            <img src="<?php echo base_url();?>asset/images/logo.png" width="35%" alt="TUBES PABW">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="oi oi-menu"></span> Menu
+        </button>
+
+        <div class="collapse navbar-collapse" >
+
+                <li class="nav-item control"><a href="<?php echo base_url(); ?>" class="nav-link icon-home">&nbspBeranda</a></li>
+                <li class="nav-item control"><a href="wisata.html" class="nav-link icon-map">&nbspWisata</a></li>
+                <li class="nav-item control"><a href="<?php echo base_url(); ?>Hotel" class="nav-link icon-hotel">&nbspHotel</a></li>
+                <li class="nav-item control"><a href="blog.html" class="nav-link icon-book">&nbspBlog</a></li>
+                <li class="nav-item control"><a href="about.html" class="nav-link icon-info">&nbspTentang</a></li>
+                <li class="nav-item control"><a href="contact.html" class="nav-link icon-phone">&nbspKontak</a></li>
+
+
+        </div>
+    </div>
+</nav>
+<!-- END nav -->
+
+
+
+<section class="ftco-section ftco-degree-bg">
+    <div class="container">
+        <h2 class="mb-4"><strong>Rincian Hotel</strong></h2>
+        <div class="row">
+            <div class="col-lg-4" >
+                <div class="row">
+                    <div class="col-sm col-md-4 col-lg ftco-animate">
+                        <div class="destination">
+                            <a href="<?php base_url('hotel/praba') ?>" class="img img-2 d-flex justify-content-center align-items-center" style="background-image: url(<?php echo base_url();?>asset/images/hotel-grand-praba.jpg);">
+                                <div class="icon d-flex justify-content-center align-items-center">
+                                    <span class="icon-search2"></span>
+                                </div>
+                            </a>
+                            <div class="text p-3">
+                                <div class="d-flex">
+                                    <div class="one">
+                                        <h3><a href="<?php base_url() ?>hotel/praba" target="_blank">Hotel Grand Praba</a></h3>
+                                        <p class="rate">
+                                            <i class="icon-star"></i>
+                                            <i class="icon-star"></i>
+                                            <i class="icon-star"></i>
+                                            <i class="icon-star"></i>
+                                            <i class="icon-star-o"></i>
+                                            <span>8 Rating</span>
+                                        </p>
+                                    </div>
+                                    <div class="two">
+                                        <span class="price per-price">Rp. 409.500<br><small>/malam</small></span>
+                                    </div>
+                                </div>
+
+                                <hr>
+                                <p class="bottom-area d-flex">
+                                    <span><i class="icon-map-o"></i> Bandar Lampung</span>
+                                    <span class="ml-auto"><a href="#">Booking</a></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div> <!-- .col-md-8 -->
+            <div class="col-sm col-md-4 ">
+                <div class="text p-3 card " >
+                    <div class="d-flex">
+                        <p><br>Hotel Bandara Syariah berjarak 5 menit berkendara dari Bandara Radin Inten II. Hotel ini memiliki restoran, serta menawarkan akses WiFi gratis di seluruh areanya. Kulkas dengan minibar tersedia di semua kamar ber-AC.
+
+Hotel Bandara Syariah berjarak 20 menit berkendara dari pusat kota Lampung, sementara Mall Lampung dapat dicapai dalam 30 menit berkendara.
+
+Kamar-kamar ber-AC hotel ini menyediakan area tempat duduk, TV kabel layar datar, dan lemari. Masing-masing kamar memiliki kamar mandi pribadi dengan sandal, perlengkapan mandi gratis, dan fasilitas shower air panas. Handuk bersih juga tersedia di kamar.
+
+Staf di meja depan yang buka 24 jam siap membantu Anda mengatur layanan binatu dan penjemputan bandara.
+
+Restoran hotel mengkhususkan diri dengan masakan Indonesia, serta menyajikan sarapan prasmanan setiap pagi. Anda juga dapat menikmati hidangan dalam kenyamanan kamar Anda. </p>
+                    </div>
+            </div>
+        </div>
+
+    </div>
+        <div class="text p-3 card col-lg-8" >
+            <div class="d-flex">
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <title>Bootstrap Example</title>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+                </head>
+                <body>
+
+                <div class="container">
+                    <ul class="nav nav-tabs ">
+                        <li class="active"><a data-toggle="tab" href="#home">Lokasi</a></li>
+                        <li><a data-toggle="tab" href="#menu1" >Ulasan</a></li>
+                        <li><a data-toggle="tab" href="#menu2 ">Info Kontak</a></li>
+                    </ul>
+
+                    <div class="tab-content ">
+                        <div id="home" class="tab-pane fade in active">
+                            <br>
+                            <h3>Lokasi :</h3>
+                            <p>Jalan Wolter Monginsidi No. 170 Teluk Betung Utara, Teluk Betung, Bandar Lampung, Provinsi Lampung, Indonesia, 35215</p>
+                        </div>
+                        <div id="menu1" class="tab-pane fade">
+                            <br>
+                            <h3>Ulasan :</h3>
+                            <p>Mantap</p>
+                        </div>
+                        <div id="menu2" class="tab-pane fade">
+                            <br>
+                            <h3>Info Kontak :</h3>
+                            <p>hotelgrandpraba@gmail.com<br></p>
+                        </div>
+                        <div id="menu3" class="tab-pane fade">
+                            <h3>Menu 3</h3>
+                            <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                        </div>
+                    </div>
+                </div>
+
+                </body>
+                </html>
+
+
+            </div>
+        </div>
+</section> <!-- .section -->
+</body>
+<!-- footer -->
+<?php $this->load->view('web/tampilan_footer'); ?>
+<!-- loader -->
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+<script src="<?php echo base_url();?>asset/js/jquery.min.js"></script>
+<script src="<?php echo base_url();?>asset/js/jquery-migrate-3.0.1.min.js"></script>
+<script src="<?php echo base_url();?>asset/js/popper.min.js"></script>
+<script src="<?php echo base_url();?>asset/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>asset/js/jquery.easing.1.3.js"></script>
+<script src="<?php echo base_url();?>asset/js/jquery.waypoints.min.js"></script>
+<script src="<?php echo base_url();?>asset/js/jquery.stellar.min.js"></script>
+<script src="<?php echo base_url();?>asset/js/owl.carousel.min.js"></script>
+<script src="<?php echo base_url();?>asset/js/jquery.magnific-popup.min.js"></script>
+<script src="<?php echo base_url();?>asset/js/aos.js"></script>
+<script src="<?php echo base_url();?>asset/js/jquery.animateNumber.min.js"></script>
+<script src="<?php echo base_url();?>asset/js/bootstrap-datepicker.js"></script>
+<script src="<?php echo base_url();?>asset/js/jquery.timepicker.min.js"></script>
+<script src="<?php echo base_url();?>asset/js/scrollax.min.js"></script>
+<script src="<?php echo base_url();?>asset/js/google-maps-aes.js"></script>
+<script src="<?php echo base_url();?>asset/js/google-map.js"></script>
+<script src="<?php echo base_url();?>asset/js/main.js"></script>
+<script type="text/javascript">$('#tabs').tabs();</script>
+<script src="<?php echo base_url();?>asset/js/jquery.min1.js"></script>//dynamic tab
+<script src="<?php echo base_url();?>asset/js/bootstrap.min1.js"></script> //dynamictab
+
+</body>
+</html>
